84e6f872b1c4f79004c23f5e9775236f7b3bfeca
Md Dilawar Hossain Farid <47987656+MdDilawarHossainFarid@users.noreply.github.com>
2023-11-13 19:27:59 +0530

    Added flight seats update api

--- src/repositories/flight-repository.js
+++ src/repositories/flight-repository.js
@@ -2,6 +2,7 @@ const { Sequelize } = require("sequelize");
 
 const CrudRepository = require("./crud-repository");
 const { Flight, Airplane, Airport, City } = require("../models");
+const db = require("../models");
 
 class FlightRepository extends CrudRepository {
   constructor() {
@@ -53,6 +54,17 @@ class FlightRepository extends CrudRepository {
       ],
     });
     return response;
+  }
+
+  async updateRemainingSeats(flightId, seats, dec = true) {
+    await db.sequelize.query(addRowLockOnFlights(flightId));
+    const flight = await Flight.findByPk(flightId);
+    if (parseInt(dec)) {
+      await flight.decrement("totalSeats", { by: seats });
+    } else {
+      await flight.increment("totalSeats", { by: seats });
+    }
+    return flight;
   }
 }
 
